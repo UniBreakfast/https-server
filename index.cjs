6bae668f0a40ea05b613706d8425dc7e95f0a905
unibreakfast <unibreakfast1@gmail.com>
2020-10-31 08:45:10 +0200

    add self-signed ssl

--- index.cjs
+++ index.cjs
@@ -4,12 +4,17 @@ const dev = module.exports = !process.env.PORT
 const port = dev ? 3000 : process.env.PORT
 if (dev) require = require('up2require')(require)
 
-
+const fs = require('fs')
 const https = require('https')
+
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
 
 const handleRequest = require('./reqHan.cjs', dev)
 
-const server = https.createServer(handleRequest)
+const server = https.createServer(options, handleRequest)
 
 server.listen(port, reportStart)
 
